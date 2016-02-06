e61b443bd40d1698323a7f596d699b0cf64a8ddb
tizianoflati <tiziano.flati@gmail.com>
2016-02-06 17:55:41 +0100

    SIlly diameter method

--- src/wsa/web/AdvancedSiteStatistics.java
+++ src/wsa/web/AdvancedSiteStatistics.java
@@ -19,7 +19,7 @@ public class AdvancedSiteStatistics {
 				graph.put(result.uri, result.links);
 	}
 	
-	public int getDistance(URI r1, URI r2)
+	public Integer getDistance(URI r1, URI r2)
 	{
 		Map<URI, Integer> distances = new HashMap<URI, Integer>();
 		
@@ -43,50 +43,67 @@ public class AdvancedSiteStatistics {
 			}
 		}
 		
-		return distances.get(r2);
+		Integer distance = distances.get(r2);
+		if(distance == null) return Integer.MAX_VALUE;
+		return distance;
 	}
 	
 	public int getDiameter()
 	{
-		Map<URI, Map<URI, Integer>> distances = new HashMap<URI, Map<URI, Integer>>();
-		for(URI u : graph.keySet())
-		{
-			Map<URI, Integer> d = distances.get(u);
-			if(d == null){d = new HashMap<URI, Integer>(); distances.put(u, d);}
-			
-			for(URI v : graph.keySet())
-			{
-				if(u == v) d.put(v, 0);
-				else if(graph.get(u).contains(v))
-				{
-					System.out.println(graph.get(u).contains(v) + "\t" + graph.get(u));
-					d.put(v, 1);
-				}
-				else d.put(v, Integer.MAX_VALUE);
-			}
-		}
-		
-		for(URI z : graph.keySet())
-			for(URI u : graph.keySet())
-				for(URI v : graph.keySet())
-				{
-					Integer uv = distances.get(u).get(v);
-					Integer uz = distances.get(u).get(z);
-					Integer zv = distances.get(z).get(v);
-					
-					if(uv == Integer.MAX_VALUE) continue;
-					if(uz == Integer.MAX_VALUE) continue;
-					if(zv == Integer.MAX_VALUE) continue;
-					
-					if(uv > uz + zv)
-						distances.get(u).put(v, uz + zv);
-				}
+//		Map<URI, Map<URI, Integer>> distances = new HashMap<URI, Map<URI, Integer>>();
+//		for(URI u : graph.keySet())
+//		{
+//			Map<URI, Integer> d = distances.get(u);
+//			if(d == null){d = new HashMap<URI, Integer>(); distances.put(u, d);}
+//			
+//			for(URI v : graph.keySet())
+//			{
+//				if(u == v) d.put(v, 0);
+//				else if(graph.get(u).contains(v))
+//				{
+//					System.out.println(graph.get(u).contains(v) + "\t" + graph.get(u));
+//					d.put(v, 1);
+//				}
+//				else d.put(v, Integer.MAX_VALUE);
+//			}
+//		}
+//		
+//		for(URI z : graph.keySet())
+//			for(URI u : graph.keySet())
+//				for(URI v : graph.keySet())
+//				{
+//					Integer uv = distances.get(u).get(v);
+//					Integer uz = distances.get(u).get(z);
+//					Integer zv = distances.get(z).get(v);
+//					
+//					if(uv == Integer.MAX_VALUE) continue;
+//					if(uz == Integer.MAX_VALUE) continue;
+//					if(zv == Integer.MAX_VALUE) continue;
+//					
+//					if(uv > uz + zv)
+//						distances.get(u).put(v, uz + zv);
+//				}
+//		
+//		Integer max = 0;
+//		for(URI u : distances.keySet())
+//			for(URI v : distances.keySet())
+//			{
+//				Integer d = distances.get(u).get(v);
+//				
+//				System.out.println("DISTANCE: " + u + "--" + v + " = " + d);
+//				
+//				if(d == Integer.MAX_VALUE) continue;
+//				else max = Math.max(max, d);
+//			}
+//		return max;
 		
 		Integer max = 0;
-		for(URI u : distances.keySet())
-			for(URI v : distances.keySet())
+		for(URI u : graph.keySet())
+			for(URI v : graph.keySet())
 			{
-				Integer d = distances.get(u).get(v);
+				if(u == v) continue;
+				
+				Integer d = getDistance(u, v);
 				if(d == Integer.MAX_VALUE) continue;
 				else max = Math.max(max, d);
 			}
